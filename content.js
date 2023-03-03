e793354ad28695dff0824a2463448cd19f79df28
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-03-03 12:38:32 +0100

    Update

---
+++ content.js
@@ -0,0 +1,9 @@
+document.head.insertAdjacentHTML("beforeend",
+    `<style>
+    .responsive_page_template_content{
+        display: block !important;
+    }
+    .adult_content_age_gate{
+        display: none !important;
+    }
+    </style>`);
