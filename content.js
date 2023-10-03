f76da8c519afd63518400cd3e3fc47501bdc73e8
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-10-03 12:36:24 +0200

    Script update

--- content.js
+++ content.js
@@ -1,9 +1,23 @@
-document.head.insertAdjacentHTML("beforeend",
-    `<style>
-    .responsive_page_template_content{
-        display: block !important;
+console.log("Content script for age check is running");
+
+function setAgeAndSubmit() {
+    let ageYearSelect = document.querySelector('#ageYear');
+    if (ageYearSelect) {
+        ageYearSelect.value = '1950';
+        console.log("Changed to 1950");
+
+        let button = document.getElementById('view_product_page_btn');
+        if (button) {
+            button.click();
+            console.log("Button clicked.");
+        } else {
+            console.log("Button with the specified ID not found!");
+        }
+    } else {
+        console.log("Age year select not found!");
     }
-    .adult_content_age_gate{
-        display: none !important;
-    }
-    </style>`);
+}
+
+setAgeAndSubmit();
+
+setTimeout(setAgeAndSubmit, 2000);
